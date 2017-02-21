39b37dcd2406d2b2ed2f8325cdc0e6a21a02478a
Gilvan Almeida <g6almeida@hotmail.com>
2017-02-21 16:26:44 -0300

    Criando o modelo gráfico

--- src/java/br/com/caelum/argentum/bean/ArgentumBean.java
+++ src/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -1,26 +1,42 @@
 package br.com.caelum.argentum.bean;
 
 
+import br.com.caelum.argentum.grafico.GeradorModeloGrafico;
+import br.com.caelum.argentum.modelo.Candlestick;
+import br.com.caelum.argentum.modelo.CandlestickFactory;
+import br.com.caelum.argentum.modelo.Negociacao;
+import br.com.caelum.argentum.modelo.SerieTemporal;
+import br.com.caelum.argentum.ws.ClienteWebService;
 import java.net.MalformedURLException;
 import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import br.com.caelum.argentum.modelo.Negociacao;
-import br.com.caelum.argentum.ws.ClienteWebService;
+import org.primefaces.model.chart.ChartModel;
 
 
 @ManagedBean
 @SessionScoped
 public class ArgentumBean {
     
-    private List<Negociacao> negociacoes;
+    private final List<Negociacao> negociacoes;
+    private final ChartModel modeloGrafico;
     
     public ArgentumBean() throws MalformedURLException{
-        negociacoes = new ClienteWebService().getNegociacoes();
+        this.negociacoes = new ClienteWebService().getNegociacoes();
+        List<Candlestick> candles = new CandlestickFactory().constroiCandles(negociacoes);
+        SerieTemporal serie = new SerieTemporal(candles);
+        
+        GeradorModeloGrafico  geradorGrafico = new GeradorModeloGrafico(serie, 2, serie.getUltimaPosicao());
+        geradorGrafico.plotaMediaMovelSimples();
+        this.modeloGrafico = geradorGrafico.getModeloGrafico();
+        
     }
     
     public List<Negociacao> getNegociacoes(){
         return negociacoes;
     }
-  
+
+    public ChartModel getModeloGrafico() {
+        return modeloGrafico;
+    } 
 }
